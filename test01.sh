4ca7c93639dbdc6e09de8ef8f56711bd119e02f2
Nehal-Mane <159645796+MaxDev-5151@users.noreply.github.com>
2024-04-27 12:46:08 +0530

    Create test01.sh

---
+++ test01.sh
@@ -0,0 +1,15 @@
+sudo apt update
+sudo wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-4+ubuntu22.04_all.deb
+sudo dpkg -i zabbix-release_6.0-4+ubuntu22.04_all.deb
+sudo apt update
+sudo apt install mysql-server
+sudo mysql
+create database zabbix character set utf8mb4 collate utf8mb4_bin;
+create user zabbix@localhost identified by 'password';
+grant all privileges on zabbix.* to zabbix@localhost;
+set global log_bin_trust_function_creators = 1;
+quit;
+sudo mysql
+show databases;
+quit;
+sudo apt install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent
